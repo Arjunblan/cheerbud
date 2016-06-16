34cab766332f36e0701c4275605ab64e9932b2dd
arjuna <arblanvillain@gmail.com>
2016-06-17 00:45:21 +0200

    ajout img et title de la video parse ds la page 2 (liste)

--- www/pay.js
+++ www/pay.js
@@ -6,3 +6,12 @@ $(document).ready(
     console.log($('body'))
   }
 )
+
+$(document).ready(
+  function(){
+    document.getElementById("video-img-cover2").setAttribute("style","background: url('http://img.youtube.com/vi/" + window.URL + "/hqdefault.jpg') 50% 50% / cover no-repeat;")
+    $('.title-video').text(window.title)
+
+    console.log($('body'))
+  }
+)
