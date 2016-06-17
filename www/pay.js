5cd06d4891da2e20e7e12cfbbc5ca55d423251b7
Arjunblan <arblanvillain@gmail.com>
2016-06-17 11:43:55 +0200

    first commit on version-max branch

--- www/pay.js
+++ www/pay.js
@@ -6,12 +6,3 @@ $(document).ready(
     console.log($('body'))
   }
 )
-
-$(document).ready(
-  function(){
-    document.getElementById("video-img-cover2").setAttribute("style","background: url('http://img.youtube.com/vi/" + window.URL + "/hqdefault.jpg') 50% 50% / cover no-repeat;")
-    $('.title-video').text(window.title)
-
-    console.log($('body'))
-  }
-)
--- www/script.js
+++ www/script.js
@@ -1,3 +1,73 @@
 var splitURL = window.location.href.split("https://www.youtube.com/watch?v=cp2Ot-QXBmw")
 
-console.log (splitURL[1]);
+// ---o Display main container
+var container = $('<div></div>');
+container.css({
+	'position': 'fixed',
+	'top': '0px',
+	'right': '0px',
+	'z-index': 9999999,
+	'width': '400px',
+	'height': '100%',
+	'background': '#FFFFFF',
+	'transition': 'transform 700ms cubic-bezier(0.190, 1.000, 0.220, 1.000) 0ms',
+	'transform': 'translate3d(100%, 0, 0)'
+});
+
+$('body').append(container);
+
+// ---o Display main button
+var youtubeTitle = $('#eow-title');
+var titleButtonSrc = chrome.extension.getURL('img/title-button.png');
+
+var titleButton = $('<img src="' + titleButtonSrc + '">');
+youtubeTitle.append(titleButton);
+
+titleButton.css({
+	'float': 'right',
+	'display': 'block',
+	'height': '30px',
+	'cursor': 'pointer'
+});
+
+titleButton.on('click', function(){
+	openPopup("");
+});
+
+// ---o Display logo on video
+var videoContainer = $('#player-api');
+var logoSrc = chrome.extension.getURL('icon.png');
+var videoIcon = $('<img src="' + logoSrc + '">');
+
+videoIcon.css({
+	'position': 'absolute',
+	'top': '15px',
+	'left': '15px',
+	'z-index': 999999,
+	'cursor': 'pointer',
+	'opacity': 0,
+	'transition': 'opacity 700ms cubic-bezier(0.190, 1.000, 0.220, 1.000) 0ms',
+});
+
+videoIcon.on('click', function(){
+	openPopup();
+});
+
+videoContainer.on('mouseenter', function(){
+	videoIcon.css('opacity', 1);
+});
+
+videoContainer.on('mouseleave', function(){
+	videoIcon.css('opacity', 0);
+});
+
+videoContainer.append(videoIcon);
+
+
+// ---o Methods
+
+function openPopup () {
+
+	container.css('transform', 'translate3d(0, 0, 0)');
+
+}
